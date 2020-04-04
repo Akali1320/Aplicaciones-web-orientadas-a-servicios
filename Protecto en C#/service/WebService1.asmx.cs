8c261083251a0ebfdc5fa92615b4164d1f7ecd83
UrielTrejoCastillo <62669651+UrielTrejoCastillo@users.noreply.github.com>
2020-04-03 19:42:21 -0600

    Proyecto c#
    Se anexa la el servicio web con conexion a base de datos

--- Protecto en C#/service/WebService1.asmx.cs
+++ Protecto en C#/service/WebService1.asmx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
@@ -26,6 +29,18 @@ namespace service
         public string fechayhora()
         {
             return DateTime.Today.ToString();
+        }
+        [WebMethod]
+        public DataSet datos()
+        {
+            String dato = "2";
+            NpgsqlConnection con = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["postgres"].ConnectionString);
+            con.Open();
+            NpgsqlCommand llamar = new NpgsqlCommand("Select nombre,curp From personas where id_persona ="+dato, con);
+            NpgsqlDataAdapter añadir = new NpgsqlDataAdapter(llamar);
+            DataSet dz = new DataSet();
+            añadir.Fill(dz);
+            return dz;
         }
     }
 }
